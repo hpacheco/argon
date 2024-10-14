cac2ac547fe0e26648035bef89382faf511e5473
Berk Özkütük <berk@ozkutuk.me>
2024-10-14 15:02:43 +0200

    refactor: remove commented out code segments, fix warnings

--- src/Argon/Parser.hs
+++ src/Argon/Parser.hs
@@ -4,7 +4,6 @@ module Argon.Parser (LModule, analyze, parseModule)
 where
 
 import Control.Exception qualified as E
-import Control.Monad (void)
 
 import GHC.LanguageExtensions qualified as GHC
 
@@ -12,24 +11,16 @@ import Argon.Loc
 import Argon.Preprocess
 import Argon.Types
 import Argon.Visitor (funcsCC)
-import Control.Monad.IO.Class (liftIO)
-import GHC.Data.StringBuffer qualified as GHC
-import GHC.Driver.Config.Parser qualified as GHC
-import GHC.Parser qualified
-import GHC.Parser.Header qualified as GHC
-import GHC.Parser.Lexer qualified as GHC
-import GHC.Utils.Error (MessageClass (..))
-import GHC.Utils.Logger qualified as GHC
-import qualified GHC.Types.SrcLoc as GHC
-import qualified Language.Haskell.Syntax as GHC
-import qualified GHC.Hs.Extension as GHC
-import qualified GHC.Driver.DynFlags as GHC
-import qualified GHC.Types.Error as GHC
-import Language.Haskell.GhclibParserEx.GHC.Parser qualified as GHC.Parser
-import qualified GHC.Utils.Outputable as GHC
-import qualified GHC.Driver.Ppr as GHC
 import Data.Maybe (mapMaybe)
-import qualified Language.Haskell.GhclibParserEx.GHC.Driver.Session as GhclibParserEx
+import GHC.Driver.DynFlags qualified as GHC
+import GHC.Driver.Ppr qualified as GHC
+import GHC.Hs.Extension qualified as GHC
+import GHC.Parser.Lexer qualified as GHC
+import GHC.Types.SrcLoc qualified as GHC
+import GHC.Utils.Outputable qualified as GHC
+import Language.Haskell.GhclibParserEx.GHC.Driver.Session qualified as GhclibParserEx
+import Language.Haskell.GhclibParserEx.GHC.Parser qualified as GHC.Parser
+import Language.Haskell.Syntax qualified as GHC
 
 -- | Type synonym for a syntax node representing a module tagged with a
 --   'SrcSpan'
@@ -48,7 +39,7 @@ analyze conf file = do
     ( do
         result <- parseModule conf file
         E.evaluate result
-      )
+    )
       `E.catch` handleExc
   let analysis = case parseResult of
         Left err -> Left err
@@ -75,62 +66,28 @@ parseModuleWithCpp
   -> FilePath
   -> IO (Either String LModule)
 parseModuleWithCpp conf cppOptions file = do
-    str <- readFile file
-    eDflags <- setExtensions (mapMaybe GhclibParserEx.readExtension conf.exts, []) file str
-    case eDflags of
-      Left err -> error err
-      Right dflags -> do
-        let useCpp = GHC.xopt GHC.Cpp dflags
-        str' <-
-          if useCpp
-            then getPreprocessedSrc cppOptions file str
-            else pure str
-        eDflags' <- setExtensions (mapMaybe GhclibParserEx.readExtension conf.exts, []) file str'
-        case eDflags' of
-          Left err -> error err
-          Right dflags' -> do
-            return $
-              case parseCode dflags' file str' of
-                GHC.PFailed ps ->
-                  Left $
-                    tagMsg
-                      (srcSpanToLoc $ GHC.mkSrcSpanPs (GHC.last_loc ps))
-                      (GHC.showSDoc dflags (GHC.ppr $ GHC.getPsMessages ps))
-                GHC.POk _ pmod -> Right pmod
+  str <- readFile file
+  eDflags <- setExtensions (mapMaybe GhclibParserEx.readExtension conf.exts, []) file str
+  case eDflags of
+    Left err -> error err
+    Right dflags -> do
+      let useCpp = GHC.xopt GHC.Cpp dflags
+      str' <-
+        if useCpp
+          then getPreprocessedSrc cppOptions file str
+          else pure str
+      eDflags' <- setExtensions (mapMaybe GhclibParserEx.readExtension conf.exts, []) file str'
+      case eDflags' of
+        Left err -> error err
+        Right dflags' -> do
+          return $
+            case parseCode dflags' file str' of
+              GHC.PFailed ps ->
+                Left $
+                  tagMsg
+                    (srcSpanToLoc $ GHC.mkSrcSpanPs (GHC.last_loc ps))
+                    (GHC.showSDoc dflags (GHC.ppr $ GHC.getPsMessages ps))
+              GHC.POk _ pmod -> Right pmod
 
 parseCode :: GHC.DynFlags -> FilePath -> String -> GHC.ParseResult LModule
 parseCode flags file = GHC.Parser.parseFile file flags
-
--- runParser :: GHC.P a -> GHC.DynFlags -> FilePath -> String -> GHC.ParseResult a
--- runParser parser flags filename str = GHC.unP parser parseState
---   where
---     location = GHC.mkRealSrcLoc (GHC.mkFastString filename) 1 1
---     buffer = GHC.stringToStringBuffer str
---     parseState = GHC.initParserState (GHC.initParserOpts flags) buffer location
-
--- initDynFlags :: GHC.GhcMonad m => Config -> FilePath -> m GHC.DynFlags
--- initDynFlags conf file = do
---   dflags0 <- GHC.getSessionDynFlags
---   (dflags1, _, _) <-
---     GHC.parseDynamicFlagsCmdLine
---       dflags0
---       [GHC.L GHC.noSrcSpan ("-X" ++ e) | e <- exts conf]
---   src_opts <- GHC.liftIO $ GHC.getOptionsFromFile (GHC.initParserOpts dflags1) file
---   (dflags2, _, _) <- GHC.parseDynamicFilePragma dflags1 (snd src_opts)
---   GHC.pushLogHookM (const customLogAction)
---   -- let dflags3 = dflags2 { GHC.log_action = customLogAction }
---   void $ GHC.setSessionDynFlags dflags2
---   return dflags2
-
-customLogAction :: GHC.LogAction
-customLogAction logFlags (MCDiagnostic severity _ _) srcSpan m =
-  case severity of
-    GHC.SevError -> throwError
-    _ -> return ()
-  where
-    throwError =
-      E.throwIO $
-        GhcParseError
-          (srcSpanToLoc srcSpan)
-          (GHC.renderWithContext (GHC.log_default_user_context logFlags) m)
-customLogAction _ _ _ _ = error "impossible?"
--- src/Argon/Preprocess.hs
+++ src/Argon/Preprocess.hs
@@ -10,21 +10,13 @@ module Argon.Preprocess
   , defaultCppOptions
   , getPreprocessedSrc
   , setExtensions
-  -- , getPreprocessedSrcDirect
   ) where
 
-import Control.Monad.IO.Class (liftIO)
-import GHC.Driver.Env.Types qualified as GHC
-import GHC.Driver.Phases qualified as GHC
-import GHC.Settings qualified as GHC
-import GHC.Types.SourceFile qualified as GHC
-import Lens.Micro (Lens')
-import Lens.Micro qualified as Lens
+import GHC.Driver.DynFlags qualified as GHC
+import GHC.LanguageExtensions.Type qualified as GHC
 import Language.Haskell.GhclibParserEx.GHC.Driver.Session (parsePragmasIntoDynFlags)
-import qualified GHC.LanguageExtensions.Type as GHC
-import qualified GHC.Driver.DynFlags as GHC
 import Language.Haskell.GhclibParserEx.GHC.Settings.Config (fakeSettings)
-import qualified Language.Preprocessor.Cpphs as Cpphs
+import Language.Preprocessor.Cpphs qualified as Cpphs
 
 data CppOptions = CppOptions
   { cppDefine :: [String]
@@ -47,44 +39,8 @@ getPreprocessedSrc :: CppOptions -> FilePath -> String -> IO String
 getPreprocessedSrc opts = Cpphs.runCpphs opts'
   where
     opts' :: Cpphs.CpphsOptions
-    opts' = Cpphs.defaultCpphsOptions
-      { Cpphs.includes = opts.cppInclude
-      -- , Cpphs.defines = opts.cppDefine
-      }
-
--- getPreprocessedSrcDirect
---   :: (GHC.GhcMonad m, MonadFail m)
---   => CppOptions
---   -> FilePath
---   -> m (String, GHC.DynFlags)
--- getPreprocessedSrcDirect cppOptions file = do
---   hscEnv <- GHC.getSession
---   let dfs = GHC.hsc_dflags hscEnv
---       newEnv = hscEnv {GHC.hsc_dflags = injectCppOptions cppOptions dfs}
---   Right (dflags', hspp_fn) <-
---     liftIO $ GHC.preprocess newEnv file Nothing (Just (GHC.Cpp GHC.HsSrcFile))
---   txt <- liftIO $ readFile hspp_fn
---   return (txt, dflags')
--- 
--- injectCppOptions :: CppOptions -> GHC.DynFlags -> GHC.DynFlags
--- injectCppOptions CppOptions {..} dflags =
---   foldr
---     addOptP
---     dflags
---     ( map mkDefine cppDefine
---         ++ map mkIncludeDir cppInclude
---         ++ map mkInclude cppFile
---     )
---   where
---     mkDefine = ("-D" ++)
---     mkIncludeDir = ("-I" ++)
---     mkInclude = ("-include" ++)
--- 
--- toolSettings :: Lens' GHC.DynFlags GHC.ToolSettings
--- toolSettings = Lens.lens GHC.toolSettings (\dflags ts -> dflags {GHC.toolSettings = ts})
--- 
--- optP :: Lens' GHC.ToolSettings [String]
--- optP = Lens.lens GHC.toolSettings_opt_P (\ts opts -> ts {GHC.toolSettings_opt_P = opts})
--- 
--- addOptP :: String -> GHC.DynFlags -> GHC.DynFlags
--- addOptP f = Lens.over (toolSettings . optP) (f :)
+    opts' =
+      Cpphs.defaultCpphsOptions
+        { Cpphs.includes = opts.cppInclude
+        -- , Cpphs.defines = opts.cppDefine
+        }
